805f552654dadf2c79b71b1de64c5509bacadaf4
andreasgehle <andreas-gehle@web.de>
2012-03-08 23:33:55 +0100

    profiler auto enable on development instead of setting hooks

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -15,10 +15,6 @@ class MY_Controller extends CI_Controller{
         // $this->load->library('user_agent');
         // if( $this->agent->is_mobile() )
         // {
-            // /*
-             // * Use my template library to set a theme for your staff
-             // *     http://philsturgeon.co.uk/code/codeigniter-template
-             // */
             // $this->template->set_theme('mobile');
         // }
 	}
