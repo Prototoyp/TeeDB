8c8824e5c9659d648b23111f072d4c6b535da6a0
Andreas Gehle <andigehle@gmail.com>
2012-05-25 17:49:21 +0200

    fixed conflicts between admin and style css

--- application/views/templates/admin/nav.php
+++ application/views/templates/admin/nav.php
@@ -1,27 +1,22 @@
 <div class="navbar navbar-fixed-top">
-  <div class="navbar-inner">
-    <div class="container">
-      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </a>
-      <?php echo anchor('', $site_title, 'class="brand"'); ?>
-      <div class="nav-collapse collapse">
-        <ul class="nav">
-          <li <?php if(preg_match('#(^admin)#', uri_string())) echo 'class="active"'; ?>>
-          	<?php echo anchor('admin','Dashboard'); ?>
-          </li>
-          <li <?php if(preg_match('#(^migrate)#', uri_string())) echo 'class="active"'; ?>>
-          	<?php echo anchor('migrate','Migrate'); ?>
-          </li>          
-          <?php foreach($modules as $module => $has_admin_support): ?>
-	          <li <?php if(preg_match('#(^'.$module.'/admin)#', uri_string())) echo 'class="active"'; ?>>
-	          	<?php echo anchor((($has_admin_support)? $module.'/admin' : uri_string().'#'), $module); ?>
-	          </li>
-          <?php endforeach; ?>
-        </ul>
-      </div>
-    </div>
-  </div>
+	<div class="navbar-inner">
+		<div class="container">
+			<?php echo anchor('', $site_title, 'class="brand"'); ?>
+			<div class="nav-collapse collapse">
+				<ul class="nav">
+					<li <?php if(preg_match('#(^admin)#', uri_string())) echo 'class="active"'; ?>>
+						<?php echo anchor('admin','Dashboard'); ?>
+					</li>
+					<li <?php if(preg_match('#(^migrate)#', uri_string())) echo 'class="active"'; ?>>
+						<?php echo anchor('migrate','Migrate'); ?>
+					</li>          
+					<?php foreach($modules as $module => $has_admin_support): ?>
+						<li <?php if(preg_match('#(^'.$module.'/admin)#', uri_string())) echo 'class="active"'; ?>>
+							<?php echo anchor((($has_admin_support)? $module.'/admin' : uri_string().'#'), $module); ?>
+						</li>
+					<?php endforeach; ?>
+				</ul>
+			</div>
+		</div>
+	</div>
 </div>
--- application/views/templates/teedb/header.php
+++ application/views/templates/teedb/header.php
@@ -21,11 +21,117 @@
 	<link rel="apple-touch-icon" type="image/x-icon" href="<?php echo base_url('assets/apple-touch-icon.png'); ?>">
 	
 	<?php if($this->load->is_loaded('auth') && $this->auth->is_admin()): ?>
-		<link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.css'); ?>">
-	    <style type="text/css">
-	      body {
-	        padding-top: 40px;
-	      }
+		<style type="text/css">
+			body {
+				padding-top: 40px;
+			}
+			.navbar-fixed-top a{
+				background: none !important;
+	      		border: 0 !important;
+			}
+	      	.navbar-fixed-top {
+				position: fixed;
+				top: 0;
+				right: 0;
+				left: 0;
+				z-index: 1030;
+			}
+			.navbar .nav {
+				position: relative;
+				left: 0;
+				display: block;
+				float: left;
+				list-style: none;
+				padding: 0;
+				margin: 10px 0;
+			}
+			.navbar {
+				overflow: visible;
+				margin-bottom: 18px;
+				font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+				font-size: 13px;
+				line-height: 18px;
+				color: #333;
+			}
+			.navbar-fixed-top .navbar-inner {
+				padding-left: 0;
+				padding-right: 0;
+				-webkit-border-radius: 0;
+				-moz-border-radius: 0;
+				border-radius: 0;
+			}
+			.navbar-inner {
+				background-color: #2C2C2C;
+				background-image: -moz-linear-gradient(top, #333, #222);
+				background-image: -ms-linear-gradient(top, #333, #222);
+				background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#333), to(#222));
+				background-image: -webkit-linear-gradient(top, #333, #222);
+				background-image: -o-linear-gradient(top, #333, #222);
+				background-image: linear-gradient(top, #333, #222);
+				background-repeat: repeat-x;
+				filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#333333', endColorstr='#222222', GradientType=0);
+				-webkit-border-radius: 4px;
+				-moz-border-radius: 4px;
+				border-radius: 4px;
+				-webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.25), inset 0 -1px 0 rgba(0, 0, 0, 0.1);
+				-moz-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.25), inset 0 -1px 0 rgba(0, 0, 0, 0.1);
+				box-shadow: 0 1px 3px rgba(0, 0, 0, 0.25), inset 0 -1px 0 rgba(0, 0, 0, 0.1);
+			}
+			.container {
+				width: 940px;
+				margin-left: auto;
+				margin-right: auto;
+			}
+			.container::before, .container::after {
+				display: table;
+				content: "";
+			}
+			.container::after {
+				clear: both;
+			}
+			.container::before, .container::after {
+				display: table;
+				content: "";
+			}
+			.navbar .brand {
+				float: left;
+				display: block;
+				padding: 8px 20px 12px;
+				margin-left: -20px;
+				font-size: 20px;
+				font-weight: 200;
+				line-height: 1;
+				color: white;
+			}
+			.collapse {
+				-webkit-transition: height 0.35s ease;
+				-moz-transition: height 0.35s ease;
+				-ms-transition: height 0.35s ease;
+				-o-transition: height 0.35s ease;
+				transition: height 0.35s ease;
+				position: relative;
+				overflow: hidden;
+				height: auto;
+			}
+			.navbar .nav > li {
+				display: block;
+				float: left;
+				line-height: 18px;
+			}
+			.navbar .nav > li > a {
+				float: none;
+				padding: 10px 10px 11px;
+				line-height: 19px;
+				color: 	#999;
+				text-decoration: none;
+				text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25);
+				font-weight: normal;
+			}
+			.navbar .nav > li > a:hover {
+				background-color: transparent;
+				color: white;
+				text-decoration: none;
+			}
 	    </style>
     <?php endif; ?>	
 
